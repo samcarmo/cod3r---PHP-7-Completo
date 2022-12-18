8d49f14f80100660d82fa6c84b5461955f23d7f7
samuelcarmo <samuelcarmo_2@hotmail.com>
2022-12-18 09:42:44 -0400

    Axios!

--- exercicios-web/ajax/server.js
+++ exercicios-web/ajax/server.js
@@ -32,9 +32,18 @@ app.post('/upload', (req, res) => {
 app.post('/formulario', (req, res) => {
     res.send({
         ...req.body,
-        id: 1
+        id: 7
     })
 })
 
+app.get('/parOuImpar', (req, res) => {
+    // req.body
+    // req.query
+    // req.params
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
 // app.get('/teste', (req, res) => res.send('Ok'))
 app.listen(8080, () => console.log('Executando...'))
