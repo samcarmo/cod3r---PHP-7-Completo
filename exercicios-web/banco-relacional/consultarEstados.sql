a1d053551be3efa8b5f4fb534df5dba8b1d161c4
samuelcarmo <samuelcarmo_2@hotmail.com>
2022-12-18 18:59:33 -0400

    Banco de Dados Relacional

---
+++ exercicios-web/banco-relacional/consultarEstados.sql
@@ -0,0 +1,12 @@
+SELECT *
+FROM estados;
+SELECT sigla,
+    nome AS 'Nome do Estado'
+FROM estados
+WHERE regiao = 'Sul';
+SELECT nome,
+    regiao,
+    populacao
+FROM estados
+WHERE populacao >= 10
+ORDER BY populacao DESC;
